36ce95a9075e60f85bce9cfc6f68fee3cd089081
unknown <tomaszhamerla@interia.pl>
2024-11-21 19:59:53 +0100

    added header nav

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLink
   ]
 })
 export class CoreModule { }
