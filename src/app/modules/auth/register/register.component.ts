4992c80c07f108f1f8c70824f9634da0c7b58df8
unknown <tomaszhamerla@interia.pl>
2024-11-23 14:58:47 +0100

    validators + post user

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../core/services/auth.service";
+import {PostUser} from "../../core/models/UserLoginDate";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -9,12 +12,47 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class RegisterComponent {
   hide = true;
   registerForm = new FormGroup({
-    email: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', {validators: [Validators.email, Validators.minLength(5), Validators.maxLength(50)], nonNullable: true}),
+    username: new FormControl('', {validators: [Validators.required], nonNullable: true}),
+    password: new FormControl('', {validators: [Validators.required], nonNullable: true}),
   })
+  errorMsg = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+  }
 
   onRegister() {
-    console.log(this.registerForm.value)
+    const userData: PostUser = this.registerForm.getRawValue();
+    this.authService.register(userData).subscribe({
+      next: () => {
+        this.router.navigate(['/logowanie'])
+      },
+      error: (err) => {
+        this.errorMsg = ' Wystapil blad'
+      }
+    })
+  }
+
+  get controls() {
+    return this.registerForm.controls;
+  }
+
+  getErrorMessages(control: FormControl) {
+    if (control.hasError('required')) {
+      return 'Musisz wpisac jakas wartosc'
+    }
+
+    if (control.hasError('minlength')) {
+      return 'Przekazano za malo ilosc znakow'
+    }
+
+    if (control.hasError('maxlength')) {
+      return 'Przekazales za duzo znakow'
+    }
+
+    return control.hasError('email') ? 'Nieprawidlowy adres email' : '';
   }
 }
