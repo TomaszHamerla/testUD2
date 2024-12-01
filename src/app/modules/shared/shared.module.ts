72b04172d312cbf5dbde1fd96c80099d12bdba9c
unknown <tomaszhamerla@interia.pl>
2024-12-01 17:27:18 +0100

    changed directive to standalone

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,15 +4,13 @@ import {MaterialModule} from "./material/material.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AlertComponent} from './components/alert/alert.component';
 import {PhoneControlComponent} from './controls/phone-control/phone-control.component';
-import { HighlightDirective } from './directives/highlight.directive';
-import { UnlessDirective } from './directives/unless.directive';
+import {UnlessDirective} from './directives/unless.directive';
 
 
 @NgModule({
   declarations: [
     AlertComponent,
     PhoneControlComponent,
-    HighlightDirective,
     UnlessDirective
   ],
   imports: [
@@ -22,7 +20,7 @@ import { UnlessDirective } from './directives/unless.directive';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule, AlertComponent,
-    PhoneControlComponent, HighlightDirective, UnlessDirective
+    PhoneControlComponent, UnlessDirective
   ]
 })
 export class SharedModule {
