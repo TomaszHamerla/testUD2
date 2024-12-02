dd597f4994542419f4c2785a9fefcb762b782ec0
unknown <tomaszhamerla@interia.pl>
2024-12-02 18:15:51 +0100

    app.modules.routes -> app.routes

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
-import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {Routes} from '@angular/router';
 import {authLoadGuard} from "./modules/core/guards/auth-load.guard";
 
-const routes: Routes = [
+export const APP_ROUTES: Routes = [
   {
     path: '',
     loadComponent: () => import('./modules/home/home.component').then((c) => c.HomeComponent)
@@ -14,10 +13,3 @@ const routes: Routes = [
   }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
-  })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app/app-routing.module';
 import {AuthModule} from './app/modules/auth/auth.module';
 import {AppComponent} from './app/app.component';
 import {importProvidersFrom} from '@angular/core';
@@ -7,10 +6,13 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {spinnerInterceptor} from "./app/modules/core/interceptors/spinner.interceptor";
 import {headerInterceptor} from "./app/modules/core/interceptors/header.interceptor";
+import {PreloadAllModules, provideRouter, withPreloading} from "@angular/router";
+import {APP_ROUTES} from "./app/app-routes";
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, AuthModule, BrowserAnimationsModule),
-    provideHttpClient(withInterceptors([spinnerInterceptor, headerInterceptor]))
+  providers: [importProvidersFrom(BrowserModule, AuthModule, BrowserAnimationsModule),
+    provideHttpClient(withInterceptors([spinnerInterceptor, headerInterceptor])),
+    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
   ]
 })
   .catch(err => console.error(err));
