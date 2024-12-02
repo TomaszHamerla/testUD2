ee9d0457c3b689fac19d2b1422c8ed1d5ba1c66c
unknown <tomaszhamerla@interia.pl>
2024-12-02 17:56:27 +0100

    deleted core module

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
-import { CoreModule } from './app/modules/core/core.module';
-import { AuthModule } from './app/modules/auth/auth.module';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app/app-routing.module';
+import {AuthModule} from './app/modules/auth/auth.module';
+import {AppComponent} from './app/app.component';
+import {importProvidersFrom} from '@angular/core';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {spinnerInterceptor} from "./app/modules/core/interceptors/spinner.interceptor";
+import {headerInterceptor} from "./app/modules/core/interceptors/header.interceptor";
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, CoreModule, AuthModule)]
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, AuthModule, BrowserAnimationsModule),
+    provideHttpClient(withInterceptors([spinnerInterceptor, headerInterceptor]))
+  ]
 })
   .catch(err => console.error(err));
